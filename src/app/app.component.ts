9bfe98444bf300d6f3c9a841ce5de5b58d80c3d6
hemanginigohel <46158643+hemanginigohel@users.noreply.github.com>
2019-01-10 14:47:37 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component ,OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'app';
+  cookieValue = 'UNKNOWN';
+constructor(private cookieService:CookieService ) { }
+
+  ngOnInit(): void {}
+  onclick(){
+    this.cookieService.set( 'Test', 'Helllo',);
+    this.cookieService.set('get','cookie');
+    this.cookieValue = this.cookieService.get('get');
+    const cookieExists: boolean = this.cookieService.check('Test');
+    console.log(cookieExists);
+    const value: string = this.cookieService.get('Test');
+    console.log(value);
+    const allCookies: {} = this.cookieService.getAll();
+    console.log(allCookies);
+    this.cookieService.set( 'name', 'hemangini',);
+    const cookie: boolean = this.cookieService.check('Test');
+    console.log(cookie);
+    this.cookieService.delete('Test');
+    const allCookie: {} = this.cookieService.getAll();
+    console.log(allCookie);
+  
+  }
+  onadd()
+  {
+    this.cookieService.set( 'name1', 'cookie1',);
+    this.cookieService.set('name2','cookie');
+    this.showall();
+  }
+  onremove(){
+    this.cookieService.delete('name1');
+    this.showall();
+  }
+  showall()
+  {
+    const allCookie: {} = this.cookieService.getAll();
+    console.log(allCookie);
+  }
+}
